4026de97e3070974caf790fed463ebce2cf9a449
Matyáš Sedmidubský <matyas@7dub.dev>
2024-12-15 20:03:44 +0100

    feat(index): Index page

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
+<link href="style.css" rel="stylesheet"/>
 <head>
 <title>Flappy bird</title>
 </head>
 <body>
-foo
+<div id="menu">
+<span id="title">Flappy bird</span>
+<button>Play</button>
+<button>Scoreboard</button>
+<button>Login</button>
+<div>
+<script>
+</script>
 </body>
 </html>
